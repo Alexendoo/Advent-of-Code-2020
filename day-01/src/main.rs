8dd0179193b3a72bca1ecb963efb27e9feae1d57
Alex Macleod <alex@macleod.io>
2020-12-01 13:16:08 +0000

    Day 01 Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,17 +1,29 @@
-use anyhow::Result;
 use itertools::Itertools;
 
-fn main() -> Result<()> {
-    let input = include_str!("input");
-    let numbers: Vec<i32> = input.lines().map(str::parse).try_collect()?;
-
+fn part_1(numbers: &[i32]) {
     let (a, b) = numbers
-        .into_iter()
+        .iter()
         .tuple_combinations()
-        .find(|(a, b)| a + b == 2020)
+        .find(|(&a, &b)| a + b == 2020)
         .unwrap();
 
     println!("{}", a * b);
+}
 
-    Ok(())
+fn part_2(numbers: &[i32]) {
+    let (a, b, c) = numbers
+        .iter()
+        .tuple_combinations()
+        .find(|(&a, &b, &c)| a + b + c == 2020)
+        .unwrap();
+
+    println!("{}", a * b * c);
+}
+
+fn main() {
+    let input = include_str!("input");
+    let numbers: Vec<i32> = input.lines().map(str::parse).try_collect().unwrap();
+
+    part_1(&numbers);
+    part_2(&numbers);
 }
