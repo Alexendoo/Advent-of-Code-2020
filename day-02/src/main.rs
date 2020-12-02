e78a5896495f5ee6b4609875441d6007d26db0ec
Alex Macleod <alex@macleod.io>
2020-12-02 13:36:54 +0000

    Day 02 part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,29 +1,68 @@
 use regex::Regex;
 
-fn part_1(input: &'static str) {
-    let pattern = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
-    let mut valid = 0;
+#[derive(Debug)]
+struct Policy {
+    min: usize,
+    max: usize,
+    ch: char,
+    password: &'static str,
+}
 
-    for line in input.lines() {
-        let captures = pattern.captures(line).unwrap();
+fn parse(line: &'static str, pattern: &Regex) -> Policy {
+    let captures = pattern.captures(line).unwrap();
 
-        let min: usize = captures[1].parse().unwrap();
-        let max: usize = captures[2].parse().unwrap();
-        let policy_char = captures[3].chars().next().unwrap();
-        let password = &captures[4];
+    let min: usize = captures[1].parse().unwrap();
+    let max: usize = captures[2].parse().unwrap();
+    let ch = captures[3].chars().next().unwrap();
+    let password = captures.get(4).unwrap().as_str();
 
-        let count = password.chars().filter(|&ch| ch == policy_char).count();
-
-        if (min..=max).contains(&count) {
-            valid += 1;
-        }
+    Policy {
+        min,
+        max,
+        ch,
+        password,
     }
+}
+
+fn part_1(policies: &[Policy]) {
+    let valid = policies
+        .iter()
+        .filter(|&policy| {
+            let range = policy.min..=policy.max;
+            let matching_chars = policy
+                .password
+                .chars()
+                .filter(|&ch| ch == policy.ch)
+                .count();
+
+            range.contains(&matching_chars)
+        })
+        .count();
+
+    println!("{}", valid);
+}
+
+fn part_2(policies: &[Policy]) {
+    let valid = policies
+        .iter()
+        .filter(|&policy| {
+            let get_char = |i| policy.password.chars().nth(i - 1).unwrap();
+
+            let first = get_char(policy.min);
+            let second = get_char(policy.max);
+
+            first != second && (first == policy.ch || second == policy.ch)
+        })
+        .count();
 
     println!("{}", valid);
 }
 
 fn main() {
     let input = include_str!("input");
+    let pattern = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
+    let policies: Vec<Policy> = input.lines().map(|line| parse(line, &pattern)).collect();
 
-    part_1(input);
+    part_1(&policies);
+    part_2(&policies);
 }
