7358181e436100e7df1f7cafa5d40d0897fc940a
Alex Macleod <alex@macleod.io>
2020-12-03 13:01:33 +0000

    Day 03 Part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -2,17 +2,28 @@ fn ascii_char_at(s: &str, index: usize) -> char {
     s.as_bytes()[index] as char
 }
 
+fn count_trees(rows: &[&str], right: usize, down: usize) -> usize {
+    let width = rows[0].len();
+
+    rows.iter()
+        .step_by(down)
+        .enumerate()
+        .map(|(index, &row)| ascii_char_at(row, (index * right) % width))
+        .filter(|&square| square == '#')
+        .count()
+}
+
 fn main() {
     let input = include_str!("input");
     let rows: Vec<&str> = input.lines().collect();
-    let width = rows[0].len();
 
-    let trees = rows
+    println!("Part 1: {}", count_trees(&rows, 3, 1));
+
+    let slopes = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let product: usize = slopes
         .iter()
-        .enumerate()
-        .map(|(index, &row)| ascii_char_at(row, (index * 3) % width))
-        .filter(|&square| square == '#')
-        .count();
+        .map(|&(right, down)| count_trees(&rows, right, down))
+        .product();
 
-    println!("{}", trees);
+    println!("Part 2: {}", product);
 }
