82dc504983a405574a909f74873f8432b4f9acfe
Alex Macleod <alex@macleod.io>
2020-12-04 13:00:53 +0000

    Day 04 Part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -9,4 +9,11 @@ fn main() {
     .unwrap();
 
     println!("Part 1: {}", re1.find_iter(input).count());
+
+    let re2 = Regex::new(
+        r"(?=(\S+\s)*byr:(19[2-9]\d|200[0-2])\s)(?=(\S+\s)*iyr:20(1\d|20)\s)(?=(\S+\s)*eyr:20(2\d|30)\s)(?=(\S+\s)*hgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)\s)(?=(\S+\s)*hcl:\#[0-9a-f]{6}\s)(?=(\S+\s)*ecl:(amb|blu|brn|gry|grn|hzl|oth)\s)(?=(\S+\s)*pid:\d{9}\s)(\S+\s)*",
+    )
+    .unwrap();
+
+    println!("Part 2: {}", re2.find_iter(input).count());
 }
