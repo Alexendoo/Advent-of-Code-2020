c13ea6d78071ae773fc7690784e8b61b9ff66a13
Alex Macleod <alex@macleod.io>
2020-12-05 12:52:49 +0000

    Day 05 Part 1

---
+++ day-05/src/main.rs
@@ -0,0 +1,54 @@
+use std::mem::size_of;
+use std::slice;
+
+#[repr(C)]
+struct Pass {
+    row_bytes: [u8; 7],
+    col_bytes: [u8; 3],
+
+    _newline: u8,
+}
+
+fn eq_b(ch: u8) -> u8 {
+    (ch == b'B') as u8
+}
+
+fn eq_r(ch: u8) -> u8 {
+    (ch == b'R') as u8
+}
+
+impl Pass {
+    fn row(&self) -> u8 {
+        let r = self.row_bytes;
+
+        eq_b(r[0]) << 6
+            | eq_b(r[1]) << 5
+            | eq_b(r[2]) << 4
+            | eq_b(r[3]) << 3
+            | eq_b(r[4]) << 2
+            | eq_b(r[5]) << 1
+            | eq_b(r[6])
+    }
+
+    fn col(&self) -> u8 {
+        let c = self.col_bytes;
+
+        eq_r(c[0]) << 2 | eq_r(c[1]) << 1 | eq_r(c[2])
+    }
+
+    fn id(&self) -> u32 {
+        (self.row() as u32) * 8 + (self.col() as u32)
+    }
+}
+
+fn main() {
+    let input = include_bytes!("input");
+    let passes = unsafe {
+        let ptr = input as *const u8 as *const Pass;
+        let len = input.len() / size_of::<Pass>();
+        slice::from_raw_parts(ptr, len)
+    };
+
+    let highest_id = passes.iter().map(Pass::id).max().unwrap();
+    println!("Part 1: {}", highest_id);
+}
