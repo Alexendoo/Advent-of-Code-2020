2a40bbc7a57d30f3113856b45179e31a22a38f94
Alex Macleod <alex@macleod.io>
2020-12-06 14:00:16 +0000

    Day 06 Part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,17 +1,29 @@
+fn bitset(answers: &str) -> u32 {
+    answers
+        .bytes()
+        .fold(0, |acc, ch| acc | (1 << ch.saturating_sub(b'a' - 1)))
+        >> 1
+}
+
 fn main() {
     let input = include_str!("input");
 
-    let sum: u32 = input
+    let sum1: u32 = input
+        .split("\n\n")
+        .map(|group| bitset(group).count_ones())
+        .sum();
+
+    println!("Part 1: {}", sum1);
+
+    let sum2: u32 = input
         .split("\n\n")
         .map(|group| {
-            let bitset = group
-                .bytes()
-                .fold(0, |acc: u32, ch| acc | (1 << ch.saturating_sub(b'a' - 1)))
-                >> 1;
-
-            bitset.count_ones()
+            group
+                .lines()
+                .fold(!0, |acc, line| acc & bitset(line))
+                .count_ones()
         })
         .sum();
 
-    println!("Part 1: {}", sum);
+    println!("Part 2: {}", sum2);
 }
