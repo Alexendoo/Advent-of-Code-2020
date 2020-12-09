7d201b5c450bbb52f36a4f4075eed05195d11156
Alex Macleod <alex@macleod.io>
2020-12-09 13:05:28 +0000

    Day 09 Part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -5,13 +5,27 @@ fn main() -> Result<()> {
     let input = include_str!("input");
     let numbers: Vec<i64> = input.lines().map(str::parse).try_collect()?;
 
-    let window = numbers.windows(26).find(|&window| {
-        let (&target, preamble) = window.split_last().unwrap();
+    let &invalid = numbers
+        .windows(26)
+        .find(|&window| {
+            let (&target, preamble) = window.split_last().unwrap();
 
-        !preamble.iter().any(|x| preamble.contains(&(target - x)))
-    });
+            !preamble.iter().any(|x| preamble.contains(&(target - x)))
+        })
+        .unwrap()
+        .last()
+        .unwrap();
 
-    println!("Part 1: {}", window.unwrap().last().unwrap());
+    println!("Part 1: {}", invalid);
+
+    let range = (2..numbers.len())
+        .flat_map(|len| numbers.windows(len))
+        .find(|&window| window.iter().sum::<i64>() == invalid)
+        .unwrap();
+
+    let (min, max) = range.iter().minmax().into_option().unwrap();
+
+    println!("Part 2: {}", min + max);
 
     Ok(())
 }
