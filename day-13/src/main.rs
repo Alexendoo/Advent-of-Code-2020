70dfc984a3fa765db9413377f9a496a28c644464
Alex Macleod <alex@macleod.io>
2020-12-13 13:18:28 +0000

    Day 13 Part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -2,15 +2,34 @@ fn main() {
     let input = include_str!("input");
     let mut lines = input.lines();
 
-    let start: u32 = lines.next().unwrap().parse().unwrap();
-    let (wait, bus) = lines
+    let start: u64 = lines.next().unwrap().parse().unwrap();
+    let buses: Vec<(u64, u64)> = lines
         .next()
         .unwrap()
         .split(',')
-        .filter_map(|s| s.parse().ok())
-        .map(|bus: u32| (bus - (start % bus), bus))
+        .enumerate()
+        .filter_map(|(i, s)| s.parse().ok().map(|bus| (i as u64, bus)))
+        .collect();
+
+    let (wait, bus) = buses
+        .iter()
+        .map(|(_, bus)| (bus - start % bus, bus))
         .min()
         .unwrap();
 
     println!("Part 1: {}", wait * bus);
+
+    let mut time: u64 = 0;
+    let mut step = 1;
+
+    for (offset, bus) in buses {
+        time = (time..)
+            .step_by(step as usize)
+            .find(|t| (t + offset) % bus == 0)
+            .unwrap();
+
+        step *= bus;
+    }
+
+    println!("Part 2: {}", time);
 }
