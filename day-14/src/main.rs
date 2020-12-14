09dbe332dc5b8d0d70f5beac62a16acea4131172
Alex Macleod <alex@macleod.io>
2020-12-14 15:20:52 +0000

    Day 14 Part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Op {
-    Mask(u64, u64),
-    Mem(usize, u64),
+    Mask(u64, u64, u64),
+    Mem(u64, u64),
 }
 
 fn parse() -> Vec<Op> {
@@ -20,16 +20,18 @@ fn parse() -> Vec<Op> {
 
                     let mut zero = !0;
                     let mut one = 0;
+                    let mut x = !0;
 
                     for (i, ch) in mask.chars().rev().enumerate() {
                         match ch {
                             '0' => zero &= !(1 << i),
                             '1' => one |= 1 << i,
-                            _ => {}
+                            'X' => x &= !(1 << i),
+                            _ => unreachable!(),
                         }
                     }
 
-                    Some(Op::Mask(zero, one))
+                    Some(Op::Mask(zero, one, x))
                 }
                 "mem" => Some(Op::Mem(
                     parts.next()?.parse().unwrap(),
@@ -42,17 +44,15 @@ fn parse() -> Vec<Op> {
         .unwrap()
 }
 
-fn main() {
-    let ops = parse();
-
-    let mut memory = HashMap::<usize, u64>::new();
+fn part_1(ops: &[Op]) {
+    let mut memory = HashMap::<u64, u64>::new();
 
     let mut zeros_mask = 0;
     let mut ones_mask = 0;
 
     for op in ops {
-        match op {
-            Op::Mask(zeroes, ones) => {
+        match *op {
+            Op::Mask(zeroes, ones, _) => {
                 zeros_mask = zeroes;
                 ones_mask = ones;
             }
@@ -63,4 +63,41 @@ fn main() {
     }
 
     println!("Part 1: {}", memory.values().sum::<u64>());
+}
+
+fn part_2(ops: &[Op]) {
+    let mut memory = HashMap::<u64, u64>::new();
+
+    let mut ones_mask = 0;
+    let mut x_mask = 0;
+
+    for op in ops {
+        match *op {
+            Op::Mask(_, ones, xs) => {
+                ones_mask = ones;
+                x_mask = xs;
+            }
+            Op::Mem(mut index, value) => {
+                index |= ones_mask;
+                index |= !x_mask;
+
+                let mut index_mask = x_mask;
+
+                for _ in 0..2u64.pow(x_mask.count_zeros()) {
+                    index_mask = x_mask | index_mask.overflowing_add(1).0;
+
+                    memory.insert(index & index_mask, value);
+                }
+            }
+        }
+    }
+
+    println!("Part 2: {}", memory.values().sum::<u64>());
+}
+
+fn main() {
+    let ops = parse();
+
+    part_1(&ops);
+    part_2(&ops);
 }
