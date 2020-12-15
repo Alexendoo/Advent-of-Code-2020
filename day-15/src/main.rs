b40cf44e969ca7e67d21c92447a729fb6afa418f
Alex Macleod <alex@macleod.io>
2020-12-15 13:33:08 +0000

    Day 15 Part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
 
-fn main() {
+fn solve(n: usize) -> u32 {
     let input = [0, 8, 15, 2, 12, 1, 4];
-
-    let mut spoken: HashMap<usize, (usize, Option<usize>)> = input[..input.len() - 1]
+    let mut spoken: HashMap<u32, (u32, Option<u32>)> = input[..input.len() - 1]
         .iter()
         .enumerate()
-        .map(|(turn, &n)| (n, (turn, None)))
+        .map(|(turn, &n)| (n, (turn as u32, None)))
         .collect();
     let mut last = *input.last().unwrap();
 
-    for turn in spoken.len()..2019 {
+    for turn in spoken.len()..(n - 1) {
+        let turn = turn as u32;
         let entry = *spoken
             .entry(last)
             .and_modify(|e| *e = (turn, Some(e.0)))
@@ -22,5 +22,10 @@ fn main() {
         };
     }
 
-    println!("Part 1: {}", last);
+    last
+}
+
+fn main() {
+    println!("Part 1: {}", solve(2020));
+    println!("Part 2: {}", solve(30000000));
 }
