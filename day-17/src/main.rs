fb94e3d97727787810531d97c960d0d1d3a00c56
Alex Macleod <alex@macleod.io>
2020-12-17 14:19:51 +0000

    Day 17 Part 2

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -2,34 +2,43 @@ use itertools::iproduct;
 use std::collections::HashSet;
 use std::ops::Range;
 
-type Coord = (i32, i32, i32);
-type Extents = [Range<i32>; 3];
+type Coord = (i32, i32, i32, i32);
+type Extents = [Range<i32>; 4];
 
-fn expand_extents(extents: &mut Extents) {
-    for range in extents {
-        range.start -= 1;
-        range.end += 1;
+fn each_coord([xs, ys, zs, ws]: Extents) -> impl Iterator<Item = Coord> {
+    iproduct!(xs, ys, zs, ws)
+}
+
+fn surrounding_coords(centre: Coord, three_d: bool) -> impl Iterator<Item = Coord> {
+    let (x, y, z, w) = centre;
+    let mut extents = [
+        (x - 1)..(x + 2),
+        (y - 1)..(y + 2),
+        (z - 1)..(z + 2),
+        (w - 1)..(w + 2),
+    ];
+
+    if three_d {
+        extents[3] = 0..1;
     }
-}
-
-fn each_coord([xs, ys, zs]: Extents) -> impl Iterator<Item = Coord> {
-    iproduct!(xs, ys, zs)
-}
-
-fn surrounding_coords(centre: Coord) -> impl Iterator<Item = Coord> {
-    let (x, y, z) = centre;
-    let extents = [(x - 1)..(x + 2), (y - 1)..(y + 2), (z - 1)..(z + 2)];
 
     each_coord(extents).filter(move |&coord| coord != centre)
 }
 
-fn main() {
+fn candidates(space: &HashSet<Coord>, three_d: bool) -> HashSet<Coord> {
+    let mut base = space.clone();
+
+    base.extend(
+        space
+            .iter()
+            .flat_map(|&coord| surrounding_coords(coord, three_d))
+    );
+
+    base
+}
+
+fn solve(three_d: bool) -> usize {
     let input = include_str!("input");
-
-    let width = input.find("\n").unwrap() as i32;
-    let height = input.lines().count() as i32;
-
-    let mut extents = [0..width, 0..height, 0..1];
 
     let mut space: HashSet<Coord> = input
         .lines()
@@ -38,17 +47,16 @@ fn main() {
             line.chars()
                 .enumerate()
                 .filter(|&(_, ch)| ch == '#')
-                .map(move |(x, _)| (x as i32, y as i32, 0))
+                .map(move |(x, _)| (x as i32, y as i32, 0, 0))
         })
         .collect();
 
     for _ in 0..6 {
-        expand_extents(&mut extents);
         let mut next_space = space.clone();
 
-        for coord in each_coord(extents.clone()) {
+        for coord in candidates(&space, three_d) {
             let active = space.contains(&coord);
-            let active_neighbours = surrounding_coords(coord)
+            let active_neighbours = surrounding_coords(coord, three_d)
                 .filter(|neighbour| space.contains(neighbour))
                 .count();
 
@@ -67,5 +75,12 @@ fn main() {
         space = next_space;
     }
 
-    println!("Part 1: {}", space.len());
+    space.len()
+}
+
+fn main() {
+    let s = std::time::Instant::now();
+    println!("Part 1: {}", solve(true));
+    println!("Part 2: {}", solve(false));
+    println!("Elapsed: {}ms", s.elapsed().as_millis());
 }
