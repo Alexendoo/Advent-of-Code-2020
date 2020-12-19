de8e1b0386049db0375ddeedb6168e6c40fd0c2e
Alex Macleod <alex@macleod.io>
2020-12-19 13:58:59 +0000

    Day 19 Part 2

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt::format;
-
 use itertools::Itertools;
 use onig::{Captures, Regex};
 
@@ -7,10 +5,7 @@ fn re(pattern: &str) -> Regex {
     Regex::new(pattern).unwrap()
 }
 
-fn main() {
-    let input = include_str!("input");
-    let (rules, messages) = input.split("\n\n").collect_tuple().unwrap();
-
+fn count_matches(rules: &str, messages: &str) -> usize {
     let without_quotes = rules.replace('"', "");
     let rule_groups = re(r"(.*): (.*)").replace_all(&without_quotes, |caps: &Captures| {
         format!("(?<c{}> {})", caps.at(1).unwrap(), caps.at(2).unwrap())
@@ -20,5 +15,18 @@ fn main() {
     });
     let pattern = format!(r"(?x) ^\g<c0>$ (?:{}){{0}}", subcalls);
 
-    println!("Part 1: {}", re(&pattern).find_iter(messages).count());
+    re(&pattern).find_iter(messages).count()
+}
+
+fn main() {
+    let input = include_str!("input");
+    let (rules, messages) = input.split("\n\n").collect_tuple().unwrap();
+
+    println!("Part 1: {}", count_matches(rules, messages));
+
+    let updated_rules = rules
+        .replace("8: 42", "8: 42 | 42 8")
+        .replace("11: 42 31", "11: 42 31 | 42 11 31");
+
+    println!("Part 2: {}", count_matches(&updated_rules, messages));
 }
